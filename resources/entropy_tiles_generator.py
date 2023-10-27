b00acd983ef1b71d41efc62b32632df1e3b46193
itvchi <jac.pikula@gmail.com>
2023-10-27 22:26:53 +0200

    Added python script to generate entropy value tile
    Tile with entropy value is generated as static image because of simple usage
    in SDL library and further use in FPGA (don't need to implement
    text to graphics converter).

---
+++ resources/entropy_tiles_generator.py
@@ -0,0 +1,17 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+TILE_COUNT = 4
+TILE_SIZE = 64
+
+img = Image.new( 'RGB', (TILE_SIZE, TILE_COUNT * TILE_SIZE), "black")
+draw = ImageDraw.Draw(img)
+font=ImageFont.truetype("Ubuntu-R.ttf", int(TILE_SIZE*0.8))
+
+for i in range(0, TILE_COUNT):
+    _, _, w, h = draw.textbbox((0, 0), str(i + 1), font=font)
+    draw.text(((TILE_SIZE-w)/2, (i * TILE_SIZE) + (TILE_SIZE-h)/2), str(i + 1), font=font)
+
+img.show()
+img.save('entropy.bmp')
